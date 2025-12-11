5483bb9af6f710519026df2de39d9b83ae8f33f7
Sajjad Shahid <chsajjadshahid@gmail.com>
2025-12-11 11:50:30 +0500

    edited

--- Routes.py
+++ Routes.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+
+from Controller.LandController import LandController
 from db import db, init_db
 from Controller.FarmerController import FarmerController
 from Model.FarmerModel import FarmerModel
@@ -14,7 +16,6 @@ from Model.NeighbourModel import NeighbourModel
 
 app = Flask(__name__)
 init_db(app)
-
 with app.app_context():
     db.create_all()
     print("created")
@@ -22,6 +23,7 @@ with app.app_context():
 @app.route('/')
 def welcome():
     return jsonify({"body": "welcome"})
+
 @app.route('/signup', methods=['POST'])
 def signup():
     return FarmerController.Signup()
@@ -33,14 +35,34 @@ def edit():
 @app.route('/login', methods=['POST'])
 def login():
     return FarmerController.Login()
+
 @app.route('/delete',methods=['DELETE'])
 def delete():
     return FarmerController.delete()
+
 @app.route('/getbyid',methods=['POST'])
 def get():
     return FarmerController.getbyid()
+
 @app.route('/getallFarmers',methods=['GET'])
 def allFarmers():
     return FarmerController.getallFarmerRecord()
+
+@app.route('/addLand',methods=['POST'])
+def addLand():
+    return LandController.AddLand()
+
+@app.route('/editLand',methods=['PUT'])
+def editLand():
+    return LandController.editLand()
+
+@app.route('/getallLands',methods=['GET'])
+def getLands():
+    return LandController.GetallLands()
+
+@app.route('/deleteLand',methods=['DELETE'])
+def deleteLand():
+    return LandController.DeleteLand()
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
